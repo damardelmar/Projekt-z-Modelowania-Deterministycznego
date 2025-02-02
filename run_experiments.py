9f04af5c4c604919cc0dcec86c42c8c6add265a1
damardelmar <danielmaher729@gmail.com>
2025-02-02 18:18:18 +0100

    Update run_experiments.py

--- run_experiments.py
+++ run_experiments.py
@@ -173,3 +173,32 @@ for m in [-10, 0, 5]:
     v.plot_heatmap(0)
     v.animate_solution()
     v.plot_heatmap(1)
+
+
+for m in [-10, 0, 5]:
+    a = Apartment(6, 4, 2 * 60, f, 2000, avg_temp_outside=m)
+    a.add_x_wall(0, 3.5, 2)
+    a.add_x_wall(4.25, 6, 2)
+
+    a.add_x_wall(3, 3.5, 3)
+    a.add_x_wall(4.25, 6, 3)
+
+    a.add_y_wall(3, 2.75, 4)
+    a.add_y_wall(3, 2, 2.25)
+
+    a.add_x_window(4, 5, 0)
+    a.add_y_window(0, .5, 1.5)
+    a.add_y_window(0, 2.5, 3.5)
+
+    a.add_heater(.25, .5, 0.5, 1.5, 0, 6, 0, 2)
+    a.add_heater(.25, .5, 2.5, 3.5, 0, 3, 2, 4)
+    a.add_heater(4, 5, .25, .5, 0, 6, 0, 2)
+
+    v = Visualiser(a)
+    s = Solver(a)
+    s.solve()
+    v.draw()
+
+    v.plot_heatmap(0)
+    v.animate_solution()
+    v.plot_heatmap(1)
